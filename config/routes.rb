69c481898a4bfc338607d4a03a0922b52d41fd45
Matthias Mau <matthias.mau@outlook.com>
2020-05-30 07:44:22 +0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :stories, only: [:index, :show, :update, :create] do
-    resources :comments, only: [:create]
-  end
+  # resources :stories, only: [:index, :show, :update, :create] do
+  #   resources :comments, only: [:create]
+  # end
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
